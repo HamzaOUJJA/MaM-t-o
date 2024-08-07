73ef7033b411879b3418ee3ca00d250dfb189221
Hamza OUJJA <hamzaoujja08@gmail.com>
2024-08-07 17:58:59 +0200

    MaMétéo_V5

--- MaMétéo_project/settings.py
+++ MaMétéo_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'y!^f73w5xi5zi17+q&hu+krknlm*j&018xfy8xx5(a)axn0%b%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles/")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MaMétéo.wsgi --log-file -
